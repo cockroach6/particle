a03b3b0976d7fd075fa7696abcee8328bfdf8d3e
qcockroach <hon.mechanic@gmail.com>
2021-05-23 00:37:31 +0000

    [box ] Bump to version number v0.1

---
+++ box/box.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# File for box utils' wrapping
+# 
+# 
+
+wd() {
+	output=$(_wd $@)
+	ret=$?
+
+	if [[ $ret -eq 0 ]];then
+		cd "$output"
+	else
+		if [[ "$output" != "" ]];then
+			echo "$output"
+		fi
+	fi
+	unset output
+	unset ret
+}
